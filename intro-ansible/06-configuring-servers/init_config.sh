a4891fd46dbdbdf771ebb2ce19442c765a4d0d05
Matt Makai <matthew.makai@gmail.com>
2018-12-18 08:52:25 -0500

    fix starting point for ch 6

--- intro-ansible/06-configuring-servers/init_config.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook -vvvv ./init_config.yml --private-key=/home/matt/do_deploy -u root -i ./hosts
--- intro-ansible/06-configuring-servers/webanddb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook -vvvv ./webanddatabase.yml --private-key=/home/matt/do_deploy -u deployer -i ./hosts
